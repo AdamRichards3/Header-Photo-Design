232d36c2e9fe7e48bf32e00674ba5a24b3b42be6
AdamRichards3 <adamwilliamrichards@gmail.com>
2020-07-28 14:23:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,19 +6,13 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(express.static('css'));
-app.use(express.json());
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, (err) => {
   console.log(`Server running on http://localhost:${PORT}`);
   if (err) console.error('Error starting server', err);
   else console.log('Server Started');
-});
-
-app.post('/post-test', (req, res) => {
-  console.log('got body:', req.body);
-  res.sendStatus(200);
 });
 
 function error(res, msg) {
